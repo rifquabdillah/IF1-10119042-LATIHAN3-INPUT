bad280bc8818c7fe82e727089a9762daa4b9771b
rifquabdillah <72897695+rifquabdillah@users.noreply.github.com>
2020-10-20 13:35:34 +0700

    Add files via upload

---
+++ Latihanpbo3.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihanpbo;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Stks
+ */
+public class Latihanpbo3 {
+    
+    public static void main(String[] args){
+        
+        System.out.println("Masukan Nama Anda: ");
+        Scanner Scanner = new Scanner (System.in);
+        
+        String nama = Scanner.next();
+        System.out.println("Nama Anda adalah" +nama);
+    }
+    
+}
